321f4f8f673e7944b88c7aa99e86f13735708e68
Richard Roxas <chardsourcex@gmail.com>
2023-03-12 17:23:26 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+function App() {
+  return (
+    <div className="text-black-200">
+      Chat Bot Turbo
+      <div className="container mx-auto border border-dashed">
+        <div className="w-1/2 h-screen">
+          <p className="text-sm">Hello, I'm ChatGPT! Ask me anything!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
