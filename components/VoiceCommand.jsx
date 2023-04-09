237b80038fd4d2d97d85f7df3f3ed9e697068105
Richard Roxas <chardsourcex@gmail.com>
2023-04-09 18:27:02 +0800

    added isTextToSpeechEnabled state

--- components/VoiceCommand.jsx
+++ components/VoiceCommand.jsx
@@ -4,8 +4,9 @@ import { useDispatch } from "react-redux";
 import { textToSpeechActions } from "../store/textToSpeech";
 import { processTextToSpeech } from "../utils/processTextToSpeech";
 import { processTextToSpeech11Labs } from "../utils/elevenLabsTTS";
+
 const VoiceCommand = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isTextToSpeechEnabled, setIsTextToSpeechEnabled] = useState(false);
   const textToSpeech = useSelector((state) => state.textToSpeech.text);
   const audioUrl = useSelector((state) => state.textToSpeech.audioUrl);
   const activeAI = useSelector((state) => state.ai.activeAI);
@@ -21,11 +22,15 @@ const VoiceCommand = () => {
   );
 
   useEffect(() => {
+    if (!isTextToSpeechEnabled) {
+      return;
+    }
+
     console.log("I got called");
     const getAudioSrc = async () => {
       const audioSrc = await processTextToSpeech(textToSpeech, activeVoice);
 
-      // TODO: Eleven Labs Implementation
+      // TODO: Eleven Labs Implementation :
       // const audioSrc = await processTextToSpeech(textToSpeech);
       console.log("I am audio src: " + audioSrc);
       return audioSrc;
@@ -35,7 +40,7 @@ const VoiceCommand = () => {
     audioSrc.then((src) => {
       dispatch(textToSpeechActions.updateAudioUrl(src));
     });
-  }, [textToSpeech]);
+  }, [textToSpeech, activeVoice, isTextToSpeechEnabled]);
 
   useEffect(() => {
     console.log("First Render");
@@ -58,6 +63,12 @@ const VoiceCommand = () => {
   const playAudio = () => {
     audioRef.current.play();
   };
+
+  const handleEnableTTS = () => {
+    setIsTextToSpeechEnabled((prevState) => !prevState);
+    console.log(isTextToSpeechEnabled);
+  };
+
   return (
     <div className="text-black w-12">
       <audio
@@ -67,21 +78,38 @@ const VoiceCommand = () => {
         style={{ display: "none" }}
       ></audio>
 
-      <button onClick={playAudio}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-12 h-12"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
-          />
-        </svg>
+      <button onClick={handleEnableTTS}>
+        {isTextToSpeechEnabled ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-12 h-12"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-12 h-12"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        )}
       </button>
     </div>
   );
