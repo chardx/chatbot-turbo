d3dd7613d7407492e061a48e56f1e95eeecd668d
Richard Roxas <chardsourcex@gmail.com>
2023-06-17 20:44:34 +0800

    fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import "../init";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "../store";
-//import for deployment fix to work w/ mozilla and edge
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
