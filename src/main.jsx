1660d511809562b681e4e1a85b13261dd0ac7424
Richard Roxas <chardsourcex@gmail.com>
2023-06-17 22:06:56 +0800

    fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "../init";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "../store";
+import "regenerator-runtime";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
