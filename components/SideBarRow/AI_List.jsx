e9796cd89aabfc65183dd88caef2d5457766870c
Richard Roxas <chardsourcex@gmail.com>
2023-06-21 18:16:36 +0800

    fix responsive

--- components/SideBarRow/AI_List.jsx
+++ components/SideBarRow/AI_List.jsx
@@ -49,7 +49,7 @@ const AI_List = () => {
           </button>
         </div>
         <div className="mt-6">
-          <ul className="divide-y divide-gray-200 overflow-y-auto">
+          <ul className="divide-y divide-gray-200">
             {listOfAI.map((role) => {
               return (
                 <AI_Role
