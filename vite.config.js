8875b495f4dd951b91e30a946cf423b6f4a292f3
Richard Roxas <chardsourcex@gmail.com>
2023-07-08 04:45:25 +0800

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,26 @@ export default defineConfig({
   plugins: [react()],
   define: {
     _global: ({}),
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
   }
 })
